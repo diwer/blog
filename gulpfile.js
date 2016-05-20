8d3401d704d73485ae5c9abc07433d2fc4d83eb0
diwer <diwers@163.com>
2016-05-20 11:51:20 +0800

    提示配置

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var markdown = require('gulp-markdown');
 
 gulp.task('markdown', function() {
     return gulp.src('**/*.md')
-        .pipe(markdown())
+        .pipe(markdown({
+			highlight: function (code) {
+				return require('highlight.js').highlightAuto(code).value;
+			}
+		}))
         .pipe(gulp.dest(function(f) {
             console.log("继续中/n");
             return f.base;
