39cdd51823237b450cf2e1c97e5cf80a9a53d62b
diwer <diwers@163.com>
2016-05-20 16:03:19 +0800

    删除 html

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
 
-gulp.task('markdown', function() {
+gulp.task('markdown', function () {
     return gulp.src('**/*.md')
         .pipe(markdown({
-			highlight: function (code) {
-				return require('highlight.js').highlightAuto(code).value;
-			}
-		}))
-        .pipe(gulp.dest(function(f) {
+            highlight: function (code, lang, callback) {
+                require('pygmentize-bundled')({ lang: lang, format: 'html' }, code, function (err, result) {
+                    callback(err, result.toString());
+                });
+            }
+        }))
+        .pipe(gulp.dest(function (f) {
             console.log("继续中/n");
             return f.base;
         }));
 });
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     gulp.watch('**/*.md', ['markdown']);
 });
